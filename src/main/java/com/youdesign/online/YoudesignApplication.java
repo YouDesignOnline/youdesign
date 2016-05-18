a6d63e3a7849f2c012378e91ff422857f365a613
Mohammad Shoaib Zahir <shoaib.zahir@timesinternet.in>
2016-05-19 01:59:59 +0530

    add project

---
+++ src/main/java/com/youdesign/online/YoudesignApplication.java
@@ -0,0 +1,12 @@
+package com.youdesign.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoudesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoudesignApplication.class, args);
+	}
+}
